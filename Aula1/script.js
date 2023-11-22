7b97bb3c5576a534832e99a6d7326cd78cb07683
RomuloNogueira84 <you@example.com>
2023-11-22 10:13:23 -0300

    novo commit

---
+++ Aula1/script.js
@@ -0,0 +1,7 @@
+let valorEmDolar = 30;
+let cotacaoDoDolar = 5.32;
+
+let valorEmReal = valorEmDolar * cotacaoDoDolar;
+valorEmReal = valorEmReal.toFixed(2);
+
+alert("O valor da cotação do Dolar é    R$"+ valorEmReal);
---
+++ Desafio1/script.js
@@ -0,0 +1,7 @@
+let valorEmDolar = 1;
+let cotacaoDoDolar = 5.32;
+
+let valorEmReal = valorEmDolar * cotacaoDoDolar;
+valorEmReal = valorEmReal.toFixed(2);
+
+alert("O valor da cotação do Dolar é    R$"+ valorEmReal);
